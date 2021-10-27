016d2892c59a303a718b4ce603fb7d9a99c3be4f
Carl <cboettig@gmail.com>
2021-10-27 23:41:17 +0000

    model map

--- score_model.py
+++ score_model.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO, SAC, DDPG, TD3, DQN
 import gym_fishing
 import gym_conservation
 import gym
@@ -7,19 +7,26 @@ import re
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
+from leaderboard import leaderboard, filehash
 # seed = 12345
 
+MODEL = {"PPO": PPO,
+         "A2C": A2C,
+         "SAC": SAC,
+         "DDPG": DDPG,
+         "TD3": TD3,
+         "DQN": DQN}
 
 model_obj = "cache/fishing-v1-A2C-team_vanilla.zip"
 
 parsed = re.search("([a-zA-Z]+\-v\d+)\-([A-Z0-9]+)\-(\w+)\.zip", model_obj)
 env_name = parsed.group(1)
-agent = parsed.group(2)
+agent_name = parsed.group(2)
 team = parsed.group(3)
 env = Monitor(gym.make(env_name))
-model = A2C.load(model_obj)
+agent = MODEL[agent_name]
+model = agent.load(model_obj)
 score = evaluate_policy(model, env, n_eval_episodes=100)
-score[0]
 
 leaderboard(agent = agent, 
             env = env_name,
